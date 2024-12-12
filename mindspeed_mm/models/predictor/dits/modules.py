72117b1d3456ae64da7aab2de675642ee07bc835
zhangxianshen <zhangjunyi8@huawei.com>
2024-12-12 01:19:23 +0000

    !480 【特性】add dits modules Merge pull request !480 from zhangxianshen/research

---
+++ mindspeed_mm/models/predictor/dits/modules.py
@@ -0,0 +1,129 @@
+import torch
+from torch import nn
+from megatron.core import mpu, tensor_parallel
+from megatron.training import get_args
+from megatron.training.arguments import core_transformer_config_from_args
+from typing import Optional, Tuple
+
+from megatron.legacy.model.rms_norm import RMSNorm
+from diffusers.models.embeddings import PixArtAlphaTextProjection, Timesteps, TimestepEmbedding
+
+
+class CombinedTimestepTextProjEmbeddings(nn.Module):
+    def __init__(self, timestep_embed_dim, embedding_dim, pooled_projection_dim):
+        super().__init__()
+
+        self.time_proj = Timesteps(num_channels=timestep_embed_dim, flip_sin_to_cos=True, downscale_freq_shift=0)
+        self.timestep_embedder = TimestepEmbedding(in_channels=timestep_embed_dim, time_embed_dim=embedding_dim)
+        self.text_embedder = PixArtAlphaTextProjection(pooled_projection_dim, embedding_dim, act_fn="silu")
+
+    def forward(self, timestep, pooled_projection):
+        timesteps_proj = self.time_proj(timestep)
+        timesteps_emb = self.timestep_embedder(timesteps_proj.to(dtype=pooled_projection.dtype))  # (N, D)
+
+        pooled_projections = self.text_embedder(pooled_projection)
+
+        conditioning = timesteps_emb + pooled_projections
+
+        return conditioning
+
+
+class AdaNorm(nn.Module):
+    r"""
+    Norm layer modified to incorporate timestep embeddings.
+
+    Parameters:
+        embedding_dim (`int`): The size of each embedding vector.
+        num_embeddings (`int`, *optional*): The size of the embeddings dictionary.
+        output_dim (`int`, *optional*):
+        norm_elementwise_affine (`bool`, defaults to `False):
+        norm_eps (`bool`, defaults to `False`):
+    """
+
+    def __init__(
+            self,
+            embedding_dim: int,
+            num_embeddings: Optional[int] = None,
+            output_dim: Optional[int] = None,
+            norm_elementwise_affine: bool = False,
+            norm_eps: float = 1e-5,
+            norm_cls: str = 'rms_norm',
+    ):
+        super().__init__()
+
+        output_dim = output_dim or embedding_dim * 2
+
+        if num_embeddings is not None:
+            self.emb = nn.Embedding(num_embeddings, embedding_dim)
+        else:
+            self.emb = None
+
+        self.silu = nn.SiLU()
+        self.linear = nn.Linear(embedding_dim, output_dim)
+        if norm_cls == 'rms_norm':
+            self.norm_cls = RMSNorm
+        elif norm_cls == 'layer_norm':
+            self.norm_cls = nn.LayerNorm
+        self.norm = self.norm_cls(
+            output_dim // 2, eps=norm_eps
+        )
+
+    def forward(
+            self, x: torch.Tensor, timestep: Optional[torch.Tensor] = None, temb: Optional[torch.Tensor] = None
+    ) -> torch.Tensor:
+        if self.emb is not None:
+            temb = self.emb(timestep)
+
+        temb = self.linear(self.silu(temb))
+        # x shape: (S B H), temb shape: (B, H)
+        shift, scale = temb.chunk(2, dim=1)
+        shift = shift[None, :, :]
+        scale = scale[None, :, :]
+
+        x = self.norm(x) * (1 + scale) + shift
+        return x
+
+
+class OpenSoraNormZero(nn.Module):
+    def __init__(
+            self,
+            timestep_embed_dim: int,
+            embedding_dim: int,
+            elementwise_affine: bool = True,
+            eps: float = 1e-5,
+            bias: bool = True,
+            norm_cls: str = 'rms_norm',
+    ) -> None:
+        super().__init__()
+        args = get_args()
+        self.sequence_parallel = args.sequence_parallel
+        config = core_transformer_config_from_args(args)
+
+        if norm_cls == 'rms_norm':
+            self.norm_cls = RMSNorm
+        elif norm_cls == 'layer_norm':
+            self.norm_cls = nn.LayerNorm
+
+        self.silu = nn.SiLU()
+        self.linear = tensor_parallel.ColumnParallelLinear(
+            timestep_embed_dim,
+            6 * embedding_dim,
+            config=config,
+            init_method=config.init_method,
+            gather_output=True
+        )
+        self.norm = self.norm_cls(embedding_dim, eps=eps, sequence_parallel=self.sequence_parallel)
+        self.norm_enc = self.norm_cls(embedding_dim, eps=eps, sequence_parallel=self.sequence_parallel)
+
+        # set label "sequence_parallel", for all_reduce the grad
+        for module in [self.norm, self.norm_enc]:
+            for param in module.parameters():
+                setattr(param, "sequence_parallel", self.sequence_parallel)
+
+    def forward(
+            self, hidden_states: torch.Tensor, encoder_hidden_states: torch.Tensor, temb: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        shift, scale, gate, enc_shift, enc_scale, enc_gate = self.linear(self.silu(temb))[0].chunk(6, dim=1)
+        hidden_states = self.norm(hidden_states) * (1 + scale)[None, :, :] + shift[None, :, :]
+        encoder_hidden_states = self.norm_enc(encoder_hidden_states) * (1 + enc_scale)[None, :, :] + enc_shift[None, :, :]
+        return hidden_states, encoder_hidden_states, gate[None, :, :], enc_gate[None, :, :]
