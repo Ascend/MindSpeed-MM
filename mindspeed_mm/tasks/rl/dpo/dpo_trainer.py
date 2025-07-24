!1297 [Bugfix]Security Update: Remove OS Info Print, Defend Formula Injection, Add Security Utils, Disable 0.0.0.0 and add public IP Merge pull request !1297 from zhangxubin/master

mindspeed_mm/tasks/rl/dpo/dpo_trainer.py
@@ -302,7 +302,7 @@ class DPOTrainer(ABC):
             global_rank = torch.distributed.get_rank()
             if loss.isnan():
                 raise ValueError(f'Rank {global_rank}: found NaN in local forward loss calculation. '
-                                 f'Device: {torch.cuda.current_device()}, node: {os.uname()[1]}')
+                                 f'Device: {torch.cuda.current_device()}')
 
         # Reduce loss for logging.
         metrics['lm loss'] = average_losses_across_data_parallel_group([loss])