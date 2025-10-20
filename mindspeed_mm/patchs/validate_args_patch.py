f02ed0b28a275cf0d376e80dcac4a428bb87afd4
MoCuishle-M <1656631289@qq.com>
2025-10-20 14:26:21 +0800

    [bugfix]Use getattr to get arguments in the validate_args function.
    Co-authored-by: MoCuishle-M<1656631289@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1614 merge master into master
    
    [bugfix]Use getattr to get arguments in the validate_args function.
    
    Created-by: MoCuishle-M
    Commit-by: MoCuishle-M
    Merged-by: ascend-robot
    Description: ## Motivation
    
    Use getattr to get arguments in the validate_args function.
    In some pipelines, such as feature extraction, custom arguments are not added to the namespace; using getattr makes the code more robust.
    
    ## Modification
    
    Use getattr to get arguments in the validate_args function.
    
    ## Self-test (Optional)
    
    If modifications to this PR may cause/fix function/accuracy/performance DTSs/issues, a self-inspection record needs to be attached.
    
    ## BC-breaking (Optional)
    
    If there are compatibility issues, such as dependencies on cann/torch_npu versions, they need to be explained in the PR.
    
    ## Checklist
    
    **Before PR**:
    
    - [ ] The new code needs to comply with the Clean Code specification.
    - [ ] The PR content is self-checked, and the expression can be clear and the writing standardized
    
    **After PR**:
    
    - [ ] CLA has been signed and all committers have signed the CLA in this PR.
    - [ ] The ci-pipeline is passed, Code Check	is passed.
    
    
    See merge request: Ascend/MindSpeed-MM!1614

--- mindspeed_mm/patchs/validate_args_patch.py
+++ mindspeed_mm/patchs/validate_args_patch.py
@@ -762,7 +762,7 @@ def validate_args(args, defaults=None):
     # Print arguments.
     _print_args("arguments", args)
 
-    ensure_valid(not (args.calculate_per_sample_loss and args.calculate_per_token_loss),
+    ensure_valid(not (getattr(args, 'calculate_per_sample_loss', False) and getattr(args, 'calculate_per_token_loss', False)),
                  ("calculate_per_sample_loss and calculate_per_token_loss cannot be used simultaneously; "
                   "only one of them can be enabled. If neither is specified, loss computation will fall back to the default behavior. "
                   "For more details, please refer to the relevant documentation: vlm-model_loss_calculate_type.md"
