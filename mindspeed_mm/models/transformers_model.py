[Bugfix]Fix incorrect loss calculation in transform_model
Co-authored-by: MoCuishle-M<1656631289@qq.com>



# message auto-generated for no-merge-commit merge:
!1695 merge master into master

[Bugfix]Fix incorrect loss calculation in transform_model

Created-by: MoCuishle-M
Commit-by: MoCuishle-M
Merged-by: ascend-robot
Description: ## Motivation

Fix incorrect loss calculation in transform_model.

## Modification

The `loss_mask` should be correctly calculated as `shift_labels > -1`.

## Self-test (Optional)

If modifications to this PR may cause/fix function/accuracy/performance DTSs/issues, a self-inspection record needs to be attached.

## BC-breaking (Optional)

If there are compatibility issues, such as dependencies on cann/torch_npu versions, they need to be explained in the PR.

## Checklist

**Before PR**:

- [ ] The new code needs to comply with the Clean Code specification.
- [ ] The PR content is self-checked, and the expression can be clear and the writing standardized

**After PR**:

- [ ] CLA has been signed and all committers have signed the CLA in this PR.
- [ ] The ci-pipeline is passed, Code Check	is passed.


See merge request: Ascend/MindSpeed-MM!1695

mindspeed_mm/models/transformers_model.py
@@ -49,7 +49,7 @@ class TransformersModel(MultiModalModule):
         loss = None
         labels = F.pad(labels, (0, 1), value=ignore_index)
         shift_labels = labels[..., 1:].contiguous()
-        loss_mask = shift_labels > 1
+        loss_mask = shift_labels > -1
 
         # The three loss calculation modes are mutually exclusive:
         # 1. Default behavior (calculate_per_sample_loss=False and calculate_per_token_loss=False):