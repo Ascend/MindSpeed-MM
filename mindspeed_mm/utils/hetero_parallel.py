7a7cc1d3e213c8f18ccb1eab02549b29e70e571e
mazhuang1234 <mazhuang21@huawei.com>
2025-11-11 18:53:08 +0800

    [Bugfix] Fallback to older Hetero-Parallel Data Comm
    Co-authored-by: mazhuang<mazhuang21@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1706 merge new-comm-group-revert into master
    
    [Bugfix] Fallback to older Hetero-Parallel Data Comm
    
    Created-by: mazhuang1234
    Commit-by: mazhuang
    Merged-by: ascend-robot
    Description: ## Motivation
    
    新版通信优化与Hetero-Parallel 流水线并行冲突，需要回调到老版Hetero数据流通信
    
    ## Modification
    
    Please briefly describe what modification is made in this PR.
    
    ## Self-test (Optional)
    
    If modifications to this PR may cause/fix function/accuracy/performance DTSs/issues, a self-inspection record needs to be attached.
    
    ## BC-breaking (Optional)
    
    If there are compatibility issues, such as dependencies on cann/torch_npu versions, they need to be explained in the PR.
    
    ## Checklist
    
    **Before PR**:
    
    - [ ] The new code needs to comply with the Clean Code specification.
    - [ ] The PR content is self-checked, and the expression can be clear and the writing standardized
    
    **After PR**:
    
    - [ ] CLA has been signed and all committers have signed the CLA in this PR.
    - [ ] The ci-pipeline is passed, Code Check	is passed.
    
    
    See merge request: Ascend/MindSpeed-MM!1706

--- mindspeed_mm/utils/hetero_parallel.py
+++ mindspeed_mm/utils/hetero_parallel.py
@@ -10,7 +10,6 @@ from mindspeed.patch_utils import MindSpeedPatchesManager as mspm
 from megatron.training import get_args, print_rank_0
 from megatron.core.parallel_state import initialize_model_parallel, is_initialized
 import megatron.core.parallel_state as mpu
-from mindspeed_mm.models.common.communications import gather_forward_split_backward
 
 
 _ParallelStatesDict = {}
@@ -22,95 +21,72 @@ def apply_hetero_parallel_hooks(model):
     if hasattr(model, 'image_encoder'):
         model.image_encoder.register_forward_pre_hook(image_encoder_forward_pre_hook)
         model.image_encoder.register_forward_hook(image_encoder_forward_hook)
-        model.image_encoder.register_full_backward_pre_hook(audio_encoder_backward_hook)
-
     if hasattr(model, 'audio_encoder'):
         model.audio_encoder.register_forward_pre_hook(audio_encoder_forward_pre_hook)
         model.audio_encoder.register_forward_hook(audio_encoder_forward_hook)
-        model.audio_encoder.register_full_backward_pre_hook(audio_encoder_backward_hook)
 
 
 def image_encoder_forward_pre_hook(module, input):
     pixel_values, image_grid_thw = input
+            
+    change_parallel_state('text_decoder')
+    pixel_values, _ = all_gather_dp_group(pixel_values, pad_dim=0, remove_padding=True)
+    image_grid_thw, _ = all_gather_dp_group(image_grid_thw)
     change_parallel_state('image_encoder')
+
     chunk_seq_lens = []
     for chunk in torch.chunk(image_grid_thw, chunks=mpu.get_data_parallel_world_size(), dim=0):
         chunk_seq_lens.append(chunk.prod(dim=1).sum())
     chunk_seq_lens = torch.stack(chunk_seq_lens).tolist()
 
-    pixel_values = DataStreamReComm.hetero_comm(
-                    pixel_values,
-                    'text_decoder',
-                    'image_encoder',
-                    cat_split_dim=0,
-                    pad_dim=0,
-                    all_lens=chunk_seq_lens
-    )
+    pixel_values = split_tensor_dp_group(pixel_values, pad_dim=0, chunk_seq_lens=chunk_seq_lens)  # [B, S]
+    image_grid_thw = split_tensor_dp_group(image_grid_thw, split_dim=0)
 
-    image_grid_thw = DataStreamReComm.hetero_comm(
-                    image_grid_thw,
-                    'text_decoder',
-                    'image_encoder',
-                    cat_split_dim=0,
-                    pad_dim=1
-    )
-    change_parallel_state('image_encoder')
     return pixel_values, image_grid_thw
 
 
 def image_encoder_forward_hook(module, input, output):    
-    output = DataStreamReComm.hetero_comm(
-                    output,
-                    'image_encoder',
-                    'text_decoder',
-                    cat_split_dim=0,
-                    pad_dim=0
-    )
+    output, all_lens = all_gather_dp_group(output, cat_dim=0, pad_dim=0, remove_padding=True)
+
     change_parallel_state('text_decoder')
+
+    chunk_seq_lens = []
+    origin_len = len(all_lens)
+    for i in range(0, origin_len, origin_len // mpu.get_data_parallel_world_size()):
+        length = sum(all_lens[i: i + origin_len // mpu.get_data_parallel_world_size()])
+        chunk_seq_lens.append(length)
+
+    output = split_tensor_dp_group(output, pad_dim=0, split_dim=0, chunk_seq_lens=chunk_seq_lens)
+
+
     return output
 
 
 def audio_encoder_forward_pre_hook(module, input):
     input_features, feature_attention_mask = input
-
-    input_features = DataStreamReComm.hetero_comm(
-                    input_features,
-                    'text_decoder',
-                    'audio_encoder',
-                    cat_split_dim=0,
-                    pad_dim=1
-    )
-    feature_attention_mask = DataStreamReComm.hetero_comm(
-                    feature_attention_mask,
-                    'text_decoder',
-                    'audio_encoder',
-                    cat_split_dim=0,
-                    pad_dim=1
-    )
+    change_parallel_state('text_decoder')
+    input_features, _ = all_gather_dp_group(input_features)
+    feature_attention_mask, _ = all_gather_dp_group(feature_attention_mask)
     change_parallel_state('audio_encoder')
+    input_features = split_tensor_dp_group(input_features)
+    feature_attention_mask = split_tensor_dp_group(feature_attention_mask)
+
     return input_features, feature_attention_mask
 
 
 def audio_encoder_forward_hook(module, input, output):
-    output = DataStreamReComm.hetero_comm(
-                    output,
-                    'audio_encoder',
-                    'text_decoder',
-                    cat_split_dim=0,
-                    pad_dim=0
-    )
+    output, all_lens = all_gather_dp_group(output, pad_token_id=0.0, cat_dim=0, pad_dim=0, remove_padding=True)
     change_parallel_state('text_decoder')
+
+    chunk_seq_lens = []
+    origin_len = len(all_lens)
+    for i in range(0, origin_len, origin_len // mpu.get_data_parallel_world_size()):
+        length = sum(all_lens[i: i + origin_len // mpu.get_data_parallel_world_size()])
+        chunk_seq_lens.append(length)
+
+    output = split_tensor_dp_group(output, pad_dim=0, split_dim=0, chunk_seq_lens=chunk_seq_lens)
+    
     return output
-
-
-def audio_encoder_backward_hook(module, input):
-    change_parallel_state('audio_encoder')
-    return input
-
-
-def image_encoder_backward_hook(module, input):
-    change_parallel_state('image_encoder')
-    return input
 
 
 def parallel_config_extract(args_dict):
@@ -201,153 +177,141 @@ if hasattr(get_mindspeed_args(), 'hetero_parallel') and get_mindspeed_args().het
     mspm.register_patch('mindspeed_mm.training.initialize_megatron',
                         initial_megatron_hetero_parallel_wrapper, force_patch=True)
     mspm.apply_patches()
+
+
+def all_gather_dp_group(tensor, 
+                        pad_token_id=None, 
+                        cat_dim=0, 
+                        pad_dim=1, 
+                        remove_padding=False,
+                        parallel_state=None,
+                        ):
+    """Gather tensors 
+        暂时只支持BSH、BD
+    """
+
+    if parallel_state is None:
+        group = mpu.get_data_parallel_group()
+        world_size = mpu.get_data_parallel_world_size()
+    else:
+        group = parallel_state['_DATA_PARALLEL_GROUP']
+        world_size = torch.distributed.get_world_size(group=group)
+    if tensor is None:
+        return None, None
+    
+    if pad_token_id is not None or remove_padding:
+        pad_token_id = 0 if pad_token_id is None else pad_token_id
+        local_len = torch.tensor([tensor.shape[pad_dim]], device='cuda')
+        all_lens = [torch.zeros_like(local_len) for _ in range(world_size)]
+
+        dist.all_gather(all_lens, local_len, group=group)
+        all_lens = [length.item() for length in all_lens]
+        max_len = max(all_lens)
+
+        pad_size = max_len - local_len
+        if pad_size > 0:
+            pad_dims = [0] * (2 * tensor.dim())
+            # pad_dims: [B, S, H], [D_left, D_right, S_left, S_right, H_left, H_right]
+            pad_dims[2 * (tensor.dim() - pad_dim) - 1] = pad_size  
+            tensor = F.pad(tensor, pad_dims, value=pad_token_id)
+
+    if tensor.requires_grad:
+        if remove_padding:
+            raise NotImplementedError('tensors that require grad and need removing padding are not implemented') 
+        output = _AllGatherDp.apply(tensor, cat_dim)
+    else:
+        gathered = [torch.zeros_like(tensor) for _ in range(world_size)]
+        dist.all_gather(gathered, tensor, group=group)
+
+        if remove_padding:
+            gathered = [g[:length] for g, length in zip(gathered, all_lens)]
+        output = torch.cat(gathered, dim=cat_dim).contiguous()
+
+    if remove_padding:
+        return output, all_lens
+    return output, None
+
+
+def split_tensor_dp_group(tensor, 
+                          split_dim=0, 
+                          pad_dim=1,
+                          chunk_seq_lens=None,
+                          all_lens=None,
+                          parallel_state=None):
+    """split tensors 
+        暂时只支持bsh
+        chunk_seq_lens: split tensor sliding chunk_seq_lens
+        all_lens: all tensor origin lens(cat_dim)
+                  if all_lens is None, split tensor per device equal or not remove padding,
+                  if all_lens is not None, remove padding intra-dp, do not remove padding inter-dp
+    """
+    
+    if parallel_state is None:
+        world_size = mpu.get_data_parallel_world_size()
+        group = mpu.get_data_parallel_group()
+    else:
+        group = parallel_state['_DATA_PARALLEL_GROUP']
+        world_size = torch.distributed.get_world_size(group=group)
+    
+    if tensor is None:
+        return None
+
+    rank = torch.distributed.get_rank(group)
+
+    if chunk_seq_lens:
+        chunk = torch.split(tensor, dim=split_dim, split_size_or_sections=chunk_seq_lens)[rank]
+    else:
+        chunks = torch.chunk(tensor, world_size, dim=split_dim)
+        chunk = chunks[rank]
+        if all_lens is not None:
+            # for not equal split, need remove padding
+            local_lens_num = len(all_lens) // world_size
+            start_idx = rank * local_lens_num
+            end_idx = start_idx + local_lens_num
+            local_lens = all_lens[start_idx: end_idx]
+            index = [slice(None)] * chunk.ndim
+            index[pad_dim] = slice(0, max(local_lens))  # for inner-mbs, not remove padding
+            chunk = chunk[tuple(index)]
+    return chunk
+
+
+class _AllGatherDp(torch.autograd.Function):
+    """
+    all gahter for dp for diff cat dim and padding dim
+    """
+    @staticmethod
+    def forward(ctx, _input, cat_dim=0):
+        group = mpu.get_data_parallel_group()
+        world_size = mpu.get_data_parallel_world_size()
+        group_rank = torch.distributed.get_rank(group)
+        ctx.world_size = world_size
+        ctx.group = group
+        ctx.group_rank = group_rank
+        ctx.cat_dim = cat_dim
+        ctx.original_batch_size = _input.shape[cat_dim]
+
+
+        gathered = [torch.zeros_like(_input) for _ in range(world_size)]
+        dist.all_gather(gathered, _input, group=group)
+        output = torch.cat(gathered, dim=cat_dim).contiguous()
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        world_size, group, group_rank, cat_dim, original_batch_size \
+            = ctx.world_size, ctx.group, ctx.group_rank, ctx.cat_dim, ctx.original_batch_size, \
+
+        start = group_rank * original_batch_size
+        end = start + original_batch_size
+
+        idx = [slice(None)] * grad_output.dim()
+        idx[cat_dim] = slice(start, end)
+        grad_input = grad_output[tuple(idx)]
+
+        return grad_input, None
     
 
 def hetero_align_config(config_inner, config_outer):
     config_inner.pipeline_model_parallel_size = config_outer.pp
     config_inner.context_parallel_size = config_outer.cp
     config_inner.tensor_model_parallel_size = config_outer.tp
-
-
-class DataStreamReComm:
-    _is_initialized = False
-    _COMM_DP_WORLD_SIZE = {}
-    _COMM_SUBGROUP_CACHE = {}
-
-    @classmethod
-    def _init(cls):
-        if cls._is_initialized:
-            return 
-        
-        for module in _HeteroParallelModules:
-            change_parallel_state(module)
-            cls._COMM_DP_WORLD_SIZE[module] = mpu.get_data_parallel_world_size()
-        cls._is_initialized = True
-
-        if cls._COMM_DP_WORLD_SIZE['text_decoder'] > cls._COMM_DP_WORLD_SIZE['audio_encoder'] \
-            or cls._COMM_DP_WORLD_SIZE['text_decoder'] > cls._COMM_DP_WORLD_SIZE['image_encoder']:
-            raise AssertionError("text decoder's DP world size should be no more than DP world size of encoders")
-    
-    @classmethod
-    def compute_comm_subgroup(cls, from_module, to_module):
-        if (from_module, to_module) in cls._COMM_SUBGROUP_CACHE:
-            return cls._COMM_SUBGROUP_CACHE[(from_module, to_module)]
-        large_DP_world_size = cls._COMM_DP_WORLD_SIZE[from_module] if cls._COMM_DP_WORLD_SIZE[from_module] > \
-                cls._COMM_DP_WORLD_SIZE[to_module] else cls._COMM_DP_WORLD_SIZE[to_module]
-        small_DP_world_size = cls._COMM_DP_WORLD_SIZE[to_module] if cls._COMM_DP_WORLD_SIZE[from_module] > \
-                cls._COMM_DP_WORLD_SIZE[to_module] else cls._COMM_DP_WORLD_SIZE[from_module]
-        
-        world_size = dist.get_world_size()
-        stride = world_size // large_DP_world_size
-        group_len = large_DP_world_size // small_DP_world_size
-        rank = dist.get_rank()
-
-        non_dp_group_ranks_list = [list(range(i * (world_size // small_DP_world_size), (i + 1) * (world_size // small_DP_world_size)))
-                                   for i in range(small_DP_world_size)]
-        
-        rank_indexes = [[stride * j + i for j in range(group_len)]for i in range(stride)]
-        ranks_list = []
-        for non_dp_group_ranks in non_dp_group_ranks_list:
-            for rank_index in rank_indexes:
-                ranks_list.append([non_dp_group_ranks[i] for i in rank_index])
-        
-        for ranks in ranks_list:
-            group = mpu.create_group(
-                ranks, 
-                group_desc='NON_DP_GROUP',
-            )
-            if rank in ranks:
-                subgroup = group
-        cls._COMM_SUBGROUP_CACHE[(from_module, to_module)] = subgroup
-
-        return subgroup
-    
-    @classmethod
-    def hetero_comm(cls,
-                    tensor,
-                    from_module,
-                    to_module,
-                    cat_split_dim=0,
-                    pad_dim=1,
-                    **kwargs):
-        cls._init()
-
-        from_dp_size = cls._COMM_DP_WORLD_SIZE[from_module]
-        to_dp_size = cls._COMM_DP_WORLD_SIZE[to_module]
-
-        if from_dp_size == to_dp_size:
-            return tensor
-        
-        subgroup_comm = cls.compute_comm_subgroup(from_module, to_module)
-
-        if from_dp_size < to_dp_size: # dp_1 < dp_2
-            return cls._hetero_split(
-                        tensor,
-                        subgroup_comm,
-                        split_dim=cat_split_dim,
-                        pad_dim=pad_dim,
-                        all_lens=kwargs.get('all_lens', None)
-                    )
-        else:
-            return cls._hetero_all_gather(
-                        tensor,
-                        subgroup_comm,
-                        cat_dim=cat_split_dim,
-                        pad_dim=pad_dim,
-                        pad_token_id=kwargs.get('pad_token_id', None)
-                    )
-        
-    @classmethod
-    def _hetero_split(cls,
-                      tensor,
-                      subgroup_comm,
-                      split_dim=0,
-                      pad_dim=1,
-                      all_lens=None):
-        world_size = dist.get_world_size(subgroup_comm)
-        rank = dist.get_rank(subgroup_comm)
-
-        if tensor.requires_grad:
-            raise AssertionError("split with tensor requiring grad is not implemented")
-        
-        if all_lens is None:
-            chunks = torch.chunk(tensor, world_size, dim=split_dim)
-            chunk = chunks[rank]
-        else:
-            step = len(all_lens) // world_size
-            all_lens = [all_lens[i: i + step] for i in range(0, len(all_lens), step)]
-
-            if split_dim == pad_dim:
-                local_lens = [sum(all_len) for all_len in all_lens]
-                chunks = torch.split(tensor, dim=split_dim, split_size_or_sections=local_lens)
-                chunk = chunks[rank]
-            else:
-                chunks = torch.chunk(tensor, world_size, dim=split_dim)
-                chunk = chunks[rank]
-                local_lens = all_lens[rank]
-
-                index = [slice(None)] * chunk.ndim
-                index[pad_dim] = slice(0, max(local_lens))
-                chunk = chunk[tuple(index)]
-        return chunk
-    
-    @classmethod
-    def _hetero_all_gather(cls,
-                           tensor,
-                           subgroup_comm,
-                           cat_dim=0,
-                           pad_dim=1,
-                           pad_token_id=None):
-        world_size = dist.get_world_size(subgroup_comm)
-        pad_token_id = 0 if pad_token_id is None else pad_token_id
-        local_len = torch.tensor([tensor.shape[pad_dim]], device='cuda')
-        all_lens = [torch.empty_like(local_len) for _ in range(world_size)]
-
-        dist.all_gather(all_lens, local_len, group=subgroup_comm)
-        all_lens = [length.item() for length in all_lens]
-        
-        output = gather_forward_split_backward(input_=tensor,
-                                               process_group=subgroup_comm,
-                                               dim=pad_dim,
-                                               gather_sizes=all_lens)
-        return output
