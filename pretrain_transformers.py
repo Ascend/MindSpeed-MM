[bugfix]correct tps calculation with cp enabled in pretrain_transformers.py
Co-authored-by: MoCuishle-M<1656631289@qq.com>



# message auto-generated for no-merge-commit merge:
!1912 merge master into master

[bugfix]correct tps calculation with cp enabled in pretrain_transformers.py

Created-by: MoCuishle-M
Commit-by: MoCuishle-M
Merged-by: ascend-robot
Description: ## Motivation

修复pretrain_transformers.py中开启CP时的TPS计算错误.

## Modification

修复pretrain_transformers.py中开启CP时的TPS计算错误. 
tokens_per_sample顾名思义，表示平均每个样本有多少有效token。当前tokens_per_sample由total_tokens计算得到，忽略了mbs大于1的情况，应该除以micro_batch_size才能得到平均每个样本有多少有效token。
开启cp的时候因为已经把序列、label都切分了，应当在dp-cp域进行all reduce。

## Self-test (Optional)

If modifications to this PR may cause/fix function/accuracy/performance DTSs/issues, a self-inspection record needs to be attached.

## BC-breaking (Optional)

If there are compatibility issues, such as dependencies on cann/torch_npu versions, they need to be explained in the PR.

## Checklist

**Before PR**:

- [x] The new code needs to comply with the Clean Code specification.
- [x] The PR content is self-checked, and the expression can be clear and the writing standardized

**After PR**:

- [x] CLA has been signed and all committers have signed the CLA in this PR.
- [x] The ci-pipeline is passed, Code Check	is passed.


See merge request: Ascend/MindSpeed-MM!1912

pretrain_transformers.py
@@ -68,8 +68,8 @@ def loss_func(output_tensor):
     
     if args.log_tps:
         dp_size = torch.distributed.get_world_size(group=mpu.get_data_parallel_group())
-        tokens_per_sample = torch.tensor(total_tokens, device=output_tensor['loss'].device) / dp_size
-        torch.distributed.all_reduce(tokens_per_sample, group=mpu.get_data_parallel_group())
+        tokens_per_sample = torch.tensor(total_tokens / args.micro_batch_size, device=output_tensor['loss'].device) / dp_size
+        torch.distributed.all_reduce(tokens_per_sample, group=mpu.get_data_parallel_group(with_context_parallel=True))
         loss_dir["tokens per sample"] = tokens_per_sample
 
     if args.calculate_per_token_loss: