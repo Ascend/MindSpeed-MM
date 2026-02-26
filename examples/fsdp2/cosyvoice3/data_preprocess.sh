c6f502a9dcda37a435b78f34394f5081b52348fb
AZe_404 <wangze62@h-partners.com>
2026-02-26 10:06:55 +0800

    [Bugfix] Bugfix for cosyvoice3 data preprocess
    Co-authored-by: AZe_404<wangze62@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2212 merge cosyvoice3_preprocess_bugfix into fsdp2_dev
    
    [Bugfix] Bugfix for cosyvoice3 data preprocess
    
    Created-by: AZe_404
    Commit-by: AZe_404
    Merged-by: ascend-robot
    Description: ## Motivation
    
    解决在部分环境中未显示使用python命令执行引入的权限问题.
    
    ## Modification
    
    将py脚本执行从文件名直接执行改为使用python显示执行，避免部分环境下文件权限问题.
    
    ## Self-test (Optional)
    
    If modifications to this PR may cause/fix function/accuracy/performance DTSs/issues, a self-inspection record needs to be attached.
    
    ## BC-breaking (Optional)
    
    If there are compatibility issues, such as dependencies on cann/torch_npu versions, they need to be explained in the PR.
    
    ## Checklist
    
    **Before PR**:
    
    - [x] The new code needs to comply with the Clean Code specification.
    - [x] The PR content is self-checked, and the expression can be clear and the writing standardized
    
    **After PR**:
    
    - [x] CLA has been signed and all committers have signed the CLA in this PR.
    - [x] The ci-pipeline is passed, Code Check	is passed.
    
    
    See merge request: Ascend/MindSpeed-MM!2212

--- examples/fsdp2/cosyvoice3/data_preprocess.sh
+++ examples/fsdp2/cosyvoice3/data_preprocess.sh
@@ -22,7 +22,7 @@ fi
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
   echo "Extract campplus speaker embedding, you will get spk2embedding.pt and utt2embedding.pt in data/$x dir"
   for x in train-clean-100; do
-    examples/fsdp2/cosyvoice3/preprocess/extract_embedding.py --dir data/$x \
+    python examples/fsdp2/cosyvoice3/preprocess/extract_embedding.py --dir data/$x \
       --onnx_path $pretrained_model_dir/campplus.onnx
   done
 fi
@@ -30,7 +30,7 @@ fi
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
   echo "Extract discrete speech token, you will get utt2speech_token.pt in data/$x dir"
   for x in train-clean-100; do
-    examples/fsdp2/cosyvoice3/preprocess/extract_speech_token.py --dir data/$x \
+    python examples/fsdp2/cosyvoice3/preprocess/extract_speech_token.py --dir data/$x \
       --onnx_path $pretrained_model_dir/speech_tokenizer_v3.onnx
   done
 fi
@@ -39,7 +39,7 @@ if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
   echo "Prepare required parquet format data, you should have prepared wav.scp/text/utt2spk/spk2utt/utt2embedding.pt/spk2embedding.pt/utt2speech_token.pt"
   for x in train-clean-100; do
     mkdir -p data/$x/parquet
-    examples/fsdp2/cosyvoice3/preprocess/make_parquet_list.py --num_utts_per_parquet 1000 \
+    python examples/fsdp2/cosyvoice3/preprocess/make_parquet_list.py --num_utts_per_parquet 1000 \
       --num_processes 10 \
       --instruct \
       --src_dir data/$x \
