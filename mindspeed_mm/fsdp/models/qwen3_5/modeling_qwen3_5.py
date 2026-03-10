64f1c1aeb813bb3e31b7563f4a56c762545687f3
vectorwhh <wanghao773@h-partners.com>
2026-03-10 21:06:01 +0800

    [Feature] Qwen3.5 27B supports chunk loss
    Co-authored-by: wanghao<wanghao773@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2237 merge qwen35_chunkloss into master
    
    [Feature] Qwen3.5 27B supports chunk loss
    
    Created-by: vectorwhh
    Commit-by: wanghao
    Merged-by: ascend-robot
    Description: ## What this PR does / why we need it?
    
    Qwen3.5 27B supports chunk loss
    
    ## Does this PR introduce any user-facing change?
    
    Please describe whether the PR will result in any user-facing usage changes. If there is related documentation, please specify its path.
    
    ## How was this patch tested?
    
    Please explain how to verify the correctness and effectiveness of this feature, as well as its usage constraints and limitations.
    
    See merge request: Ascend/MindSpeed-MM!2237

--- mindspeed_mm/fsdp/models/qwen3_5/modeling_qwen3_5.py
+++ mindspeed_mm/fsdp/models/qwen3_5/modeling_qwen3_5.py
@@ -1968,11 +1968,15 @@ class Qwen3_5ForConditionalGeneration(Qwen3_5PreTrainedModel, GenerationMixin):
 
         # Only compute necessary logits, and do not upcast them to float if we are not computing the loss
         slice_indices = slice(-logits_to_keep, None) if isinstance(logits_to_keep, int) else logits_to_keep
-        logits = self.lm_head(hidden_states[:, slice_indices, :])
+        if getattr(self, "enable_chunk_loss", False):
+            logits = None
+            loss = self.lm_head(hidden_states[:, slice_indices, :], self.loss_function)
+        else:
+            logits = self.lm_head(hidden_states[:, slice_indices, :])
 
-        loss = None
-        if labels is not None:
-            loss = self.loss_function(logits=logits, labels=labels, vocab_size=self.config.text_config.vocab_size)
+            loss = None
+            if labels is not None:
+                loss = self.loss_function(logits=logits, labels=labels, vocab_size=self.config.text_config.vocab_size)
 
         return Qwen3_5CausalLMOutputWithPast(
             loss=loss,
