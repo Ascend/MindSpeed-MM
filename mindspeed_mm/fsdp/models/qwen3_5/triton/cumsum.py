4854e033de72579081a644bdd4484b50c0dcb706
rewindz <bianzheng8@huawei.com>
2026-04-23 17:28:26 +0800

    fix: GDN support headnum=64 for qwen3.5 397B & fix triton3.2.1 comatibility issue
    Co-authored-by: BianZheng<bianzheng8@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2424 merge fix260423 into master
    
    fix: GDN support headnum=64 for qwen3.5 397B & fix triton3.2.1 comatibility issue
    
    Created-by: LinMingZhe
    Commit-by: BianZheng
    Merged-by: ascend-robot
    Description: ## What this PR does / why we need it?
    
    support headnum=64 for qwen3.5 397B & fix triton3.2.1 comatibility issue
    
    ## Does this PR introduce any user-facing change?
    
    NA
    
    ## How was this patch tested?
    
    UT TA3.2.0/3.2.1 CANN8.5.2
    
    See merge request: Ascend/MindSpeed-MM!2424

--- mindspeed_mm/fsdp/models/qwen3_5/triton/cumsum.py
+++ mindspeed_mm/fsdp/models/qwen3_5/triton/cumsum.py
@@ -83,7 +83,7 @@ def chunk_local_cumsum_scalar(
         raise ValueError(
             f"chunk_size must be a power of 2, chunk_size is{chunk_size}"
         )
-    BT = triton.next_power_of_2((1 << 17) // (H * chunk_size))
+    BT = triton.next_power_of_2((1 << 18) // (H * chunk_size))
     chunk_indices = prepare_chunk_indices(cu_seqlens, BT) if cu_seqlens is not None else None
     NT = triton.cdiv(T, BT) if cu_seqlens is None else len(chunk_indices)
     g_org, g = g, torch.empty_like(g, dtype=output_dtype or g.dtype)
