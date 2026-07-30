9842d1427d92c7b50129ef8b963a7b6facd13676
ascend-robot <zhongyuanke@huawei.com>
2026-07-30 20:51:10 +0800

    修复 qwen25vl_7B mbridge 特性报错的问题
    Co-authored-by: bonjour_rzq1998<ruanzeqi1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2906 merge feature/fix-mbridge-qwen25vl into master
    
    修复 qwen25vl_7B mbridge 特性报错的问题
    
    Created-by: bonjour_rzq1998
    Commit-by: bonjour_rzq1998
    Merged-by: ascend-robot
    Description: What this PR does / why we need it?
    1、修改bridge/models/qwen_vl/__init__.py中对于异常情况的容忍范围。
    
    Does this PR introduce any user-facing change?
    不涉及
    
    How was this patch tested?
    不涉及
    
    See merge request: Ascend/MindSpeed-MM!2906

--- bridge/models/qwen_vl/__init__.py
+++ bridge/models/qwen_vl/__init__.py
@@ -11,8 +11,13 @@ except ImportError as exc:
     exc_name = getattr(exc, "name", "") or ""
     exc_msg = str(exc)
 
+    known_optional_deps = (
+        "mindspeed.te",
+        "fla_npu",
+    )
+
     if not (
-        exc_name.startswith("mindspeed.te")
+        exc_name.startswith(known_optional_deps)
         or "mindspeed.te.pytorch.utils" in exc_msg
         or "get_tensor_model_parallel_group_if_none" in exc_msg
     ):
