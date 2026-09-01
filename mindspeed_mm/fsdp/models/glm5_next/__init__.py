ffb855ed62e435cc105c8395c058020f3d82aff4
liuxi_ <liuxi75@huawei.com>
2026-09-01 20:12:57 +0800

    [feature] add glm 5.3 flash code
    Co-authored-by: liuxi_<liuxi75@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3058 merge glm53_code into master
    
    [feature] add glm 5.3 flash code
    
    Created-by: liuxi_
    Commit-by: liuxi_
    Merged-by: ascend-robot
    Description: What this PR does / why we need it?
    支持glm5.3 flash模型
    
    Does this PR introduce any user-facing change?
    新增glm5.3 flash模型文件
    
    How was this patch tested?
    已在本地通过模型验证, 训练loss下降正常,功能正常
    算子替换前后loss差异符合要求.
    新增算子用例后续补充, 补充后关联
    
    超大PR说明:新模型开箱上库模型代码, 一次合入
    
    See merge request: Ascend/MindSpeed-MM!3058

---
+++ mindspeed_mm/fsdp/models/glm5_next/__init__.py
@@ -0,0 +1,16 @@
+from .configuration_glm5_next import Glm5NextConfig, Glm5NextTextConfig
+from .modeling_glm5_next import (
+    Glm5NextForConditionalGeneration,
+    Glm5NextModel,
+    Glm5NextPreTrainedModel,
+    Glm5NextTextModel,
+)
+
+__all__ = [
+    "Glm5NextConfig",
+    "Glm5NextTextConfig",
+    "Glm5NextForConditionalGeneration",
+    "Glm5NextModel",
+    "Glm5NextPreTrainedModel",
+    "Glm5NextTextModel",
+]
