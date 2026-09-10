7b240d042681c0f48b65e198c2c49a185d38cc14
ye_qm <yeqiangmao@huawei.com>
2026-09-10 17:40:06 +0800

    feat(qwen3omni): support A5 on qwen3omni
    Co-authored-by: yeqm<yeqiangmao@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !3124 merge a5_qwen3omni into master
    
    feat(qwen3omni): support A5 on qwen3omni
    
    Created-by: ye_qm
    Commit-by: yeqm
    Merged-by: ascend-robot
    Description: ## What this PR does / why we need it?
    
    Add a single-node 8-NPU launch script for Qwen3-Omni training on A5.
    
    The script also collects the average iteration time and samples per second from the training log.
    
    ## Does this PR introduce any user-facing change?
    
    Yes. A new launch script is added:
    
    `examples/qwen3omni/finetune_qwen3omni_v1_A5.sh`
    
    Users can modify the distributed parameters and use it to launch Qwen3-Omni training on A5.
    
    ## How was this patch tested?
    
    Test on A5
    
    See merge request: Ascend/MindSpeed-MM!3124

---
+++ examples/qwen3omni/finetune_qwen3omni_v1_A5.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# 根据实际情况修改 ascend-toolkit 路径
+source /usr/local/Ascend/cann/set_env.sh
+export NON_MEGATRON=true
+export MULTI_STREAM_MEMORY_REUSE=2
+export ASCEND_LAUNCH_BLOCKING=0
+export HCCL_CONNECT_TIMEOUT=1200
+export TASK_QUEUE_ENABLE=2
+export ACLNN_CACHE_LIMIT=100000
+export CPU_AFFINITY_CONF=1
+export PYTORCH_NPU_ALLOC_CONF=expandable_segments:True
+
+# 当前脚本单机8卡，请根据实际情况指定设备
+NPUS_PER_NODE=8
+MASTER_ADDR=<master_ip_address>
+MASTER_PORT=6000
+NNODES=1
+NODE_RANK=0
+
+DISTRIBUTED_ARGS="
+    --nproc_per_node $NPUS_PER_NODE \
+    --nnodes $NNODES \
+    --node_rank $NODE_RANK \
+    --master_addr $MASTER_ADDR \
+    --master_port $MASTER_PORT
+"
+
+logfile=$(date +%Y%m%d)_$(date +%H%M%S)
+mkdir -p logs
+torchrun $DISTRIBUTED_ARGS mindspeed_mm/fsdp/train/trainer.py \
+    examples/qwen3omni/qwen3omni_config_v1.yaml \
+    2>&1 | tee logs/train_${logfile}.log
+
+STEP_TIME=`grep "elapsed time per iteration" logs/train_${logfile}.log | awk -F 'elapsed time per iteration [(]ms[)]:' '{print$2}' | awk -F '|' '{print$1}' | head -n 200 | tail -n 100 | awk '{sum+=$1} END {if (NR != 0) printf("%.1f",sum/NR)}'`
+GBS=`grep "global batch size" logs/train_${logfile}.log | awk -F 'global batch size:' '{print$2}' | awk -F '|' '{print$1}' | head -n 1 | awk '{print $1}'`
+SAMPLES_PER_SECOND=`awk 'BEGIN{printf "%.3f\n", '${GBS}'*1000/'${STEP_TIME}'}'`
+echo "Elapsed Time Per iteration (ms): $STEP_TIME" | tee -a logs/train_${logfile}.log
+echo "Average Samples per Second: $SAMPLES_PER_SECOND" | tee -a logs/train_${logfile}.log
